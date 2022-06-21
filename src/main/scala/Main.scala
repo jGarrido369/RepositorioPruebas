c5c098635b1ac50015c7aa5d5d54e75af83e2afa
hubsantander <jgarridog24@gmail.com>
2022-06-21 12:58:55 +0200

    primera version del repositorio de prruebas

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,24 @@
+object Main {
+  def main(args: Array[String]): Unit = {
+
+  }
+
+  val tup=(1,2)
+  println(tup)
+
+  var list=Array.range(1,51,1)
+  for ( x <- list ) {
+    print( " " + x )
+  };
+println("")
+  var cl1 : List[String]= List("nombre","direcçao","tlf","email" )
+  var cl2 : List[String]= List("nombre2","direcçao2","tlf2","email2" )
+var mapa: Map[Int, List[String]]=Map()
+  mapa += (1->cl1)
+  mapa += (2->cl2)
+
+  println(mapa)
+
+
+}
+
